68839c6efb91fee612977ba2d11502126f5f2449
ericadu <erica.c.du@gmail.com>
2015-05-01 17:56:08 -0400

    Added routes and function for random URL

--- quartirs/quartirs_app/urls.py
+++ quartirs/quartirs_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+  url(r'^[0-9a-zA-Z ]+$', views.check_in, name='check_in'),
 ]
--- quartirs/quartirs_app/views.py
+++ quartirs/quartirs_app/views.py
@@ -6,3 +6,7 @@ from django.template import RequestContext, loader
 # Create your views here.
 def index(request):
 	return render(request, 'quartirs_app/index.html', {})
+
+def check_in(request):
+  context = {}
+  return render(request, 'quartirs_app/check_in.html', context)
