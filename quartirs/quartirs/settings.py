8158d1e3f68e8ba06a55d0840947e7d40d516ce4
Jenny Astrachan <jennya@mit.edu>
2015-05-01 10:34:46 -0400

    adding qrcode library and basic code on url

--- quartirs/quartirs/settings.py
+++ quartirs/quartirs/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'quartirs_app',
+    'qrcode',
 )
 
 MIDDLEWARE_CLASSES = (
--- quartirs/quartirs/urls.py
+++ quartirs/quartirs/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'quartirs.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^quartirs_app/', include('quartirs_app.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
